ed9299b2b014cc4703805f888545b68571560823
Teglas Levente <levente.teglasz@gmail.com>
2025-04-13 18:33:21 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './shared/menu/menu.component';
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
-import { MatSliderModule } from '@angular/material/slider';
-
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    RouterLink,
-    MenuComponent,
-    MatSliderModule
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'diszkont';
-
-  onToggleSidenav(sidenav: MatSidenav){
-    sidenav.toggle();
-  }
 }
-
